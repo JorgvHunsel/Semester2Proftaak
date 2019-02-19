277c1ac70e26c2cfb8940ce4dbe8935aee55d522
Jorg Van Hunsel <jorg.van.hunsel@gmail.com>
2019-02-19 09:55:21 +0100

    Created the UI layout for hulpvraag aanvragen

--- Fontys.PTS2.Prototype/Form1.Designer.cs
+++ Fontys.PTS2.Prototype/Form1.Designer.cs
@@ -28,20 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblSubject = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.tbSubject = new System.Windows.Forms.TextBox();
+            this.tbDescription = new System.Windows.Forms.TextBox();
+            this.btnRequest = new System.Windows.Forms.Button();
+            this.cbUrgent = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(43, 42);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(505, 58);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Hulpvraag aanvragen";
+            // 
+            // lblSubject
+            // 
+            this.lblSubject.AutoSize = true;
+            this.lblSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubject.Location = new System.Drawing.Point(48, 183);
+            this.lblSubject.Name = "lblSubject";
+            this.lblSubject.Size = new System.Drawing.Size(136, 29);
+            this.lblSubject.TabIndex = 1;
+            this.lblSubject.Text = "Onderwerp";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescription.Location = new System.Drawing.Point(48, 285);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(151, 29);
+            this.lblDescription.TabIndex = 2;
+            this.lblDescription.Text = "Beschrijving";
+            // 
+            // tbSubject
+            // 
+            this.tbSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSubject.Location = new System.Drawing.Point(236, 191);
+            this.tbSubject.Name = "tbSubject";
+            this.tbSubject.Size = new System.Drawing.Size(307, 36);
+            this.tbSubject.TabIndex = 3;
+            // 
+            // tbDescription
+            // 
+            this.tbDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDescription.Location = new System.Drawing.Point(236, 285);
+            this.tbDescription.Multiline = true;
+            this.tbDescription.Name = "tbDescription";
+            this.tbDescription.Size = new System.Drawing.Size(710, 76);
+            this.tbDescription.TabIndex = 4;
+            // 
+            // btnRequest
+            // 
+            this.btnRequest.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRequest.Location = new System.Drawing.Point(631, 452);
+            this.btnRequest.Name = "btnRequest";
+            this.btnRequest.Size = new System.Drawing.Size(315, 69);
+            this.btnRequest.TabIndex = 5;
+            this.btnRequest.Text = "Aanvragen";
+            this.btnRequest.UseVisualStyleBackColor = true;
+            // 
+            // cbUrgent
+            // 
+            this.cbUrgent.AutoSize = true;
+            this.cbUrgent.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbUrgent.Location = new System.Drawing.Point(236, 400);
+            this.cbUrgent.Name = "cbUrgent";
+            this.cbUrgent.Size = new System.Drawing.Size(130, 33);
+            this.cbUrgent.TabIndex = 7;
+            this.cbUrgent.Text = "Urgentie";
+            this.cbUrgent.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.cbUrgent);
+            this.Controls.Add(this.btnRequest);
+            this.Controls.Add(this.tbDescription);
+            this.Controls.Add(this.tbSubject);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblSubject);
+            this.Controls.Add(this.lblTitle);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Hulpvraag aanmaken";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblSubject;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.TextBox tbSubject;
+        private System.Windows.Forms.TextBox tbDescription;
+        private System.Windows.Forms.Button btnRequest;
+        private System.Windows.Forms.CheckBox cbUrgent;
     }
 }
 
