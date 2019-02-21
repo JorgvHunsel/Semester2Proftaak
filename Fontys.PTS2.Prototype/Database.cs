d72efc1a0da3da6ae935c8332fe67f189e5a26e4
Jorg Van Hunsel <jorg.van.hunsel@gmail.com>
2019-02-21 10:24:55 +0100

    Made the database class with connection string

---
+++ Fontys.PTS2.Prototype/Database.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fontys.PTS2.Prototype
+{
+    class Database
+    {
+        //const string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\jorg_\source\repos\Webshop prototype\Webshop prototype\Database1.mdf;Integrated Security=True";
+
+        private const string connectionString = @"Data Source=mssql.fhict.local;Initial Catalog=dbi423244;User ID=dbi423244;Password=********;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private SqlConnection conn = new SqlConnection(connectionString);
+    }
+}
