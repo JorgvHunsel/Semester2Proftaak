6478c8b187cb652d01138a476039e1a7dd4cbe24
Dennis Hong <340272@student.fontys.nl>
2019-02-21 11:37:31 +0100

    Added Category class

--- Fontys.PTS2.Prototype/Classes/Category.cs
+++ Fontys.PTS2.Prototype/Classes/Category.cs
@@ -8,5 +8,29 @@ namespace Fontys.PTS2.Prototype.Classes
 {
     class Category
     {
+        private int CategoryId { get; set; }
+        private string Name { get; set; }
+        private string Description { get; set; }
+
+        public void AddCategory(string name)
+        {
+            //Add new Category
+        }
+
+        public void EditCategory(Category c)
+        {
+            //Edit c.Description
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            return null;
+            //Returns all categories in a list
+        }
+
+        public void DeleteCategory(Category c)
+        {
+            //Delete the category tha's been given as parameter
+        }
     }
 }
