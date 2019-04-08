572e32fcba93d88559e80a82d5e5b832214e4713
Jesse Oosterwijk <I398785@fhict.nl>
2019-04-08 09:58:55 +0200

    made new form

--- Fontys.PTS2.Prototype/View/CareRecipient/FormReactionQuestionCareRecipient.Designer.cs
+++ Fontys.PTS2.Prototype/View/CareRecipient/FormReactionQuestionCareRecipient.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lvQuestionReaction = new System.Windows.Forms.ListView();
+            this.SenderName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ReactionContent = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.TimeStamp = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lvQuestionReaction
+            // 
+            this.lvQuestionReaction.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.SenderName,
+            this.ReactionContent,
+            this.TimeStamp});
+            this.lvQuestionReaction.Location = new System.Drawing.Point(12, 58);
+            this.lvQuestionReaction.Name = "lvQuestionReaction";
+            this.lvQuestionReaction.Size = new System.Drawing.Size(662, 380);
+            this.lvQuestionReaction.TabIndex = 0;
+            this.lvQuestionReaction.UseCompatibleStateImageBehavior = false;
+            this.lvQuestionReaction.View = System.Windows.Forms.View.Details;
+            // 
+            // SenderName
+            // 
+            this.SenderName.Text = "Afzender";
+            // 
+            // ReactionContent
+            // 
+            this.ReactionContent.Text = "Inhoud";
+            this.ReactionContent.Width = 50;
+            // 
+            // TimeStamp
+            // 
+            this.TimeStamp.Text = "Tijd van reactie";
+            this.TimeStamp.Width = 99;
+            // 
+            // FormReactionQuestionCareRecipient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(796, 450);
+            this.Controls.Add(this.lvQuestionReaction);
+            this.Name = "FormReactionQuestionCareRecipient";
             this.Text = "FormReactionQuestionCareRecipient";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView lvQuestionReaction;
+        private System.Windows.Forms.ColumnHeader SenderName;
+        private System.Windows.Forms.ColumnHeader ReactionContent;
+        private System.Windows.Forms.ColumnHeader TimeStamp;
     }
 }
